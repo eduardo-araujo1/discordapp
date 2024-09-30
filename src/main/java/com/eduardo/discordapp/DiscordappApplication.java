0d0149222a2da84cac729050bf242af6d0963d20
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-09-30 19:53:22 -0300

    first commit

---
+++ src/main/java/com/eduardo/discordapp/DiscordappApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.discordapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/discordapp/DiscordappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.discordapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
